e4c3e9cd31b6e2f4ba29e1d9a70238336e151d94
Lenovo <Lenovo@192.168.1.12>
2020-04-04 16:05:31 +0800

    Program challenge

---
+++ src/program/ReverseString.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ReverseString {
+    
+    public void reverseWordInMyString(String str){
+        
+        /*
+        The split() method of String class splits 
+        a string in several strings based on the
+        delimiter passed as an argument to it
+        */
+        
+        String[] words = str.split(" ");
+        String reversedString = "";
+        for(int i = 0; i < words.length; i++){
+        
+        String word = words[i];
+        String reversedWord = "";
+        for(int j = word.length() - 1; j >= 0; j--){
+            
+            /*
+            The charAt() function returns the character
+            at the given position in a string
+            */
+            reversedWord = reversedWord + word.charAt(j);
+        }
+        reversedString = reversedString + reversedWord;
+    }
+        System.out.println(str);
+        System.out.println(reversedString);
+    }
+    
+    public static void main(String[]args){
+        
+        ReverseString obj = new ReverseString();
+        obj.reverseWordInMyString("Welcome to BeginnersBook");
+        obj.reverseWordInMyString("This is an easy Java Program");
+    }
+}
